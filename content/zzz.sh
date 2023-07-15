6e121395b68b9404767647480e854917a61a90a2
t52w11qa <139205802+t52w11qa@users.noreply.github.com>
2023-07-15 15:51:44 +0800

    Create zzz.sh

---
+++ content/zzz.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget -q --no-cache -P /etc/mosdns -O b.yaml https://t52w11qa.neocities.org/t52w11qa/b.txt &&
+wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/baiym.txt &&
+wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/hosts.txt &&
+wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/cdxym.txt &&
+wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/cdxzf.txt &&
+wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/guowaiym.txt &&
+wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/cnym.txt &&
+wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/heiym.txt &&
+wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/heiymzzz.txt
