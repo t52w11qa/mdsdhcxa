6d82969001ffc05d587943ccee26bdc656785622
t52w11qa <139205802+t52w11qa@users.noreply.github.com>
2023-07-15 17:10:03 +0800

    Update zzz.sh

--- content/zzz.sh
+++ content/zzz.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-wget -q --no-cache -P /etc/mosdns -O b.yaml https://t52w11qa.neocities.org/t52w11qa/b.txt &&
 wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/baiym.txt &&
 wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/hosts.txt &&
 wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/cdxym.txt &&
@@ -8,4 +7,5 @@ wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/cdxzf.
 wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/guowaiym.txt &&
 wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/cnym.txt &&
 wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/heiym.txt &&
-wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/heiymzzz.txt
+wget -q --no-cache -P /etc/mosdns https://t52w11qa.neocities.org/t52w11qa/heiymzzz.txt &&
+wget -q --no-cache -O /etc/mosdns/b.yaml https://t52w11qa.neocities.org/t52w11qa/b.txt
